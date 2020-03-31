aa690753ea47915476f137cef98142962e34f660
Tim Buergel <tim.burgel@epfl.ch>
2020-03-31 10:04:26 +0200

    Project created

---
+++ Miniprojet/main.c
@@ -0,0 +1,68 @@
+/*
+ * main.c
+ *
+ *  Created on: 31.03.2020
+ *      Author: Tim Bürgel
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <motors.h>
+#include <camera/po8030.h>
+#include <chprintf.h>
+
+
+
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    //starts the serial communication
+    serial_start();
+    //start the USB communication
+    usb_start();
+    //starts the camera
+    dcmi_start();
+	po8030_start();
+	//inits the motors
+	motors_init();
+
+    /* Infinite loop. */
+    while (1) {
+    	//waits 1 second
+        chThdSleepMilliseconds(1000);
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
---
+++ Miniprojet/main.h
@@ -0,0 +1,23 @@
+/*
+ * main.h
+ *
+ *  Created on: 31.03.2020
+ *      Author: Tim Bürgel
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "camera/dcmi_camera.h"
+#include "msgbus/messagebus.h"
+#include "parameter/parameter.h"
+
+
+/** Robot wide IPC bus. */
+extern messagebus_t bus;
+
+extern parameter_namespace_t parameter_root;
+
+
+
+#endif /* MAIN_H_ */
