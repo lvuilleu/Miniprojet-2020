8f4c827ccc585f1f587215855b3cf428f93be9e4
Tim Buergel <tim.burgel@epfl.ch>
2020-04-07 15:58:43 +0200

    XYAngle calc and GITIGNORE

--- Miniprojet/pos_control.c
+++ Miniprojet/pos_control.c
@@ -8,6 +8,8 @@
 #include "ch.h"
 #include "hal.h"
 
+#include <math.h>
+
 
 #include <pos_control.h>
 #include <motors.h>
@@ -51,24 +53,24 @@ typedef enum { // evtl. CLKW and ACLKW rotation as states => scan_speed zu scan_
 #define SIDESTEP_DIST		100 // mm
 
 struct robot_t{
-	int16_t pos_x;
-	int16_t pos_y;
+	float pos_x; //Ich ha chli angst vor relativ grosse rundigsfehler wenn mir die position all 50ms update chönnt sich das evlt recht uffeaddiere...
+	float pos_y; //Drumm float
 	float angle;
 	uint8_t motor_state;
 };
 
 static struct robot_t robot;
 
-int32_t save_appr_steps(void){
+/*int32_t save_appr_steps(void){
 	int32_t appr_steps = (left_motor_get_pos() + right_motor_get_pos()) / 2; // int/int!
 	left_motor_set_pos(appr_steps);
 	right_motor_set_pos(appr_steps);
 	return appr_steps;
-}
+}*/
+//Comment
 
-float get_angle(int32_t appr_steps){
-	float r_angle = (float)((right_motor_get_pos() - appr_steps)*2*WHEEL_PERIMETER)/(float)(WHEEL_DISTANCE*NSTEP_ONE_TURN);
-	return r_angle;
+float get_angle(void){
+	return ((float)(right_motor_get_pos()*2*WHEEL_PERIMETER)/(float)(WHEEL_DISTANCE*NSTEP_ONE_TURN));
 }
 
 void set_motors(uint8_t motors_state, int speed){
@@ -76,20 +78,36 @@ void set_motors(uint8_t motors_state, int speed){
 		case STRAIGHT:
 			right_motor_set_speed(speed);
 			left_motor_set_speed(speed);
+			robot.motor_state = STRAIGHT;
 			break;
 		case ROTATION:
 			right_motor_set_speed(speed);
 			left_motor_set_speed(-speed);
+			robot.motor_state = ROTATION;
 			break;
 		case STOP: // isch chli schï¿½ner als default findi, odr hetts do e ï¿½berlegig dehinter gha?
 			right_motor_set_speed(0);
 			left_motor_set_speed(0);
-
+			robot.motor_state = STOP;
+			break;
 	}
 }
 
 void new_coord_and_angle(void){
-
+	switch(robot.motor_state){
+	case ROTATION:
+		robot.angle += get_angle();
+		break;
+	case STRAIGHT: ;
+		uint8_t distance= right_motor_get_pos()/NSTEP_ONE_TURN*WHEEL_PERIMETER;
+		robot.pos_y += cos(robot.angle)*distance;
+		robot.pos_x += sin(robot.angle)*distance;
+		break;
+	case STOP:
+		break;
+	right_motor_set_pos(0);
+	left_motor_set_pos(0);
+	}
 }
 
 void calculate_target_pos(void) {
@@ -112,17 +130,22 @@ static THD_FUNCTION(PosControl, arg) {
     static uint16_t y_target = 0;
     static uint16_t x_target = 0;
 
+    robot.pos_x = 0;
+    robot.pos_y = 0;
+    robot.angle = 0;
+    robot.motor_state = STOP;
+
     while(1)
     {
     		time = chVTGetSystemTime();
-
-    		//Calculate new position
-    		new_coord_and_angle();
 
     		if(get_selector() > 8 && state == WAIT)
     			state = SCAN;
     		if(get_selector() < 8)
     			state = WAIT;
+
+    		//Calculate new position
+			new_coord_and_angle();
 
     		if(DEBUG)
     		{
@@ -172,7 +195,6 @@ static THD_FUNCTION(PosControl, arg) {
 			case DETECT_COLOR:
 				// teste wo dr zylinder im vrgliich zur kamera isch, wohrschinli jo eher links denn chï¿½nne mr niid die zentrale pixel uslese sondern bruuche en offset
 				take_image();
-				//Jetzt gohts en moment bis s bild fertig isch, entweder warte odr scho losfahre abr denn hemmr s risiko dass s bild verwacklet/ am falsche ort misst
 				state = SIDESTEP;
 				y_target = robot.pos_y + SIDESTEP_DIST;
 				break;
